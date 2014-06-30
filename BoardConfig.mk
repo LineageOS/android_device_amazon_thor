78fc7e9fdb2405f2f053140bb2d1d8909fdf583e
Hashcode <hashcode0f@gmail.com>
2014-06-30 15:12:13 -0700

    add defconfig and dt location

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,9 +4,12 @@
 # inherit from the proprietary version
 -include vendor/amazon/hdx-common/BoardConfigVendor.mk
 
+# Kernel config
+TARGET_KERNEL_CONFIG := thor_android_defconfig
+BOARD_MKBOOTIMG_ARGS += --dt device/amazon/thor/dt.img
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := thor
 
 # TWRP
 DEVICE_RESOLUTION := 1200x1920
-
