ebfefc9e612e1fcb0e0f374b8a7d95cd3fac1bad
cpasjuste <cpasjuste@gmail.com>
2014-11-18 09:36:55 +0100

    ...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,8 +5,9 @@
 -include vendor/amazon/hdx-common/BoardConfigVendor.mk
 
 # Kernel config
-TARGET_KERNEL_CONFIG := thor_defconfig
+TARGET_KERNEL_CONFIG := thor_cyanogenmod_defconfig
 BOARD_MKBOOTIMG_ARGS += --dt device/amazon/thor/dt-13.3.2.4.img
+#dt-13.4.1.1.img
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := thor
