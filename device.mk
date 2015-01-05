b171aea82298793e4934111227e200619164eff4
Cpasjuste <cpasjuste@sd-70635.dedibox.fr>
2015-01-05 16:13:31 +0100

    add cuber img signing - thanks vortox

--- device.mk
+++ device.mk
@@ -9,13 +9,7 @@ $(call inherit-product-if-exists, vendor/amazon/thor/thor-vendor.mk)
 # Device overlay
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/amazon/thor/hacks/zImage
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-# hdx old bootloader dtb compatibility fix + bootloader exploit patch ?
+# hdx old bootloader dtb compatibility fix + bootloader exploit patch
 # override bootimg to manually append propietary dtb extracted from 4.5.2 kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/amazon/thor/mkboot.mk
 
--- mkboot.mk
+++ mkboot.mk
@@ -5,7 +5,9 @@ BOARD_MKBOOTIMG_ARGS := --kernel $(PRODUCT_OUT)/kernel.dt --kernel_offset 0x0000
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	@echo -e ${CL_CYN}"Building hdx hacked boot image: $@"${CL_RST}
 	cat $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/zImage device/amazon/thor/thor-dt.img > $(PRODUCT_OUT)/kernel.dt
-	$(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	./device/amazon/thor/Cuber/Cuber -sign $@ $@.signed
+	$(hide) mv $@.signed $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made hdx hacked boot image: $@"${CL_RST}
 
@@ -14,5 +16,7 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	@echo -e ${CL_CYN}"Building hdx hacked recovery image: $@"${CL_RST}
 	cat $(KERNEL_OUT)/arch/$(TARGET_ARCH)/boot/zImage device/amazon/thor/thor-dt.img > $(PRODUCT_OUT)/kernel.dt
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	./device/amazon/thor/Cuber/Cuber -sign $@ $@.signed
+	$(hide) mv $@.signed $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made hdx hacked recovery image: $@"${CL_RST}
