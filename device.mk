9c08104b7546e94c8b3986c211268e84e31ed36e
cpasjuste <cpasjuste@gmail.com>
2015-02-26 09:56:28 +0100

    fully disable radio support for now

--- device.mk
+++ device.mk
@@ -1,5 +1,20 @@
 DEVICE_FOLDER := device/amazon/thor
 
+# Set dirty regions off
+PRODUCT_PROPERTY_OVERRIDES += \
+	hwui.render_dirty_regions=false
+
+# RIL turn off
+PRODUCT_PROPERTY_OVERRIDES += \
+	keyguard.no_require_sim=1 \
+	ro.radio.use-ppp=no \
+	ro.config.nocheckin=yes \
+	ro.radio.noril=yes
+
+# wifi-only device
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.carrier=wifi-only
+
 # Inherit from hdx
 $(call inherit-product, device/amazon/hdx-common/hdx-common.mk)
 
@@ -8,8 +23,4 @@ $(call inherit-product-if-exists, vendor/amazon/thor/thor-vendor.mk)
 
 # Device overlay
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay
-
-
-
-
 
--- full_thor.mk
+++ full_thor.mk
@@ -1,12 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/amazon/thor/device.mk)
 
-# wifi-only device
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=wifi-only
-
-PRODUCT_CHARACTERISTICS := tablet,nosdcard
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 PRODUCT_NAME := full_thor
