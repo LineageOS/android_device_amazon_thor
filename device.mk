77f73519780d96aacce2ccc4475225b80f6bd3ad
Hashcode <hashcode0f@gmail.com>
2015-06-05 08:32:00 -0700

    thor: add wpa_supplicant base configuration from stock
    Change-Id: I3f0f6167628e3757b03d593d66beced7292ba127

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ ADDITIONAL_BUILD_PROPERTIES += \
 
 # configs
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_ath6kl.conf:system/etc/wifi/wpa_supplicant.conf \
 	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
