2ab59b3dae3228fdb27f2bf8bbecdd4ddf3e055a
Hashcode <hashcode0f@gmail.com>
2016-07-19 17:27:15 -0700

    remove ignored wpa_supplicant configuration

--- device.mk
+++ device.mk
@@ -27,7 +27,6 @@ ADDITIONAL_BUILD_PROPERTIES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_ath6kl.conf:system/etc/wifi/wpa_supplicant.conf \
 	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
 	$(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
 	$(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
