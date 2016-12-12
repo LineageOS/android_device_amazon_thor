9ee59f940c6fba26ad904ab54aef41323be7ccff
Hashcode <hashcode0f@gmail.com>
2016-12-12 13:42:38 -0800

    TEMP: selinux back to permissive mode for now
    Change-Id: I0bf0ecba3a90fd80e25e804ff21ba2ff8f9843d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := thor_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 
 # inherit from common hdx
 -include device/amazon/hdx-common/BoardConfigCommon.mk
